96e6b84454a9c2761aa306cdef5836e2ca5335b9
zopelee <zopelee@gmail.com>
2015-08-26 19:13:08 +0800

    initial commit.

---
+++ src/setting.js
@@ -0,0 +1,22 @@
+var Setting = {
+  wave: {
+    tag: 'wave',
+    color: 0xCCCCCC,
+    opacity: 1
+  },
+  innerWave: {
+    tag: 'innerWave',
+    color: 0x000000,
+    opacity: 0.3,
+    ratio: 0.5
+  },
+  progress: {
+    tag: 'progress',
+    color: 0x000000,
+    opacity: 1
+  },
+  shapeMapping: {
+    scaleFactor: 0.6,
+    heightFactor: 1
+  }
+}
